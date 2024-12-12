484569fd7b9218f967237d6428a661b6dfcdae64
Stephen Okpalaononuju <stephenokpala@gmail.com>
2024-12-12 11:25:59 +0100

    chore(todo): add Dockerfile

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const APP_PORT = process.env.PORT;
+const APP_PORT = process.env.PORT || 8850;
 
 app.listen(APP_PORT, () => {
   console.log(`Server is running on PORT ${APP_PORT}`);
